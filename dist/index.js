92398c403a6d61906411aea4b9c9e662ea4bffb1
Len Lester <lenster921@gmail.com>
2023-03-02 14:35:26 -0800

    configured eslint and prettier

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
+const test = 1;
 app.get('/', (_req, res) => {
     return res.send('Express Typescript on Vercel');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
 
 const app = express()
 const port = process.env.PORT || 8000
+const test = 1
 
 app.get('/', (_req: Request, res: Response) => {
   return res.send('Express Typescript on Vercel')
 })
-
 app.get('/ping', (_req: Request, res: Response) => {
   return res.send('pong 🏓')
 })
